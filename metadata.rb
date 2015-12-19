81e4832b5c14b1d1ff347c9b4b730acdf30603b9
Steve Nolen <technolengy@gmail.com>
2015-12-18 21:10:41 -0800

    ugh

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'technolengy@gmail.com'
 license 'Apache 2.0'
 description 'Installs/Configures cens-web'
 long_description 'Installs/Configures cens-web'
-version '0.0.9'
+version '0.0.10'
 
 %w(ubuntu).each do |os|
   supports os
