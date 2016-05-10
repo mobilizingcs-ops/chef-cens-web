f61aabebcbc132e3c07ef05d89f7f376735c023c
Steve Nolen <technolengy@gmail.com>
2016-05-10 15:08:06 -0700

    disable some legacy apps

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'technolengy@gmail.com'
 license 'Apache 2.0'
 description 'Installs/Configures cens-web'
 long_description 'Installs/Configures cens-web'
-version '0.0.10'
+version '0.0.11'
 
 %w(ubuntu).each do |os|
   supports os
